be174cb5c05bc01d1cb132b264abd371a3fc7267
Erik <erkmgwr@gmail.com>
2022-04-12 10:32:00 -0300

    Comments

--- popup/js/index.js
+++ popup/js/index.js
@@ -1,16 +1,28 @@
 class Fetch{
 
     render(){
+        
+        //Instancia do XMLHttpRequest
         const httpRequest = new XMLHttpRequest();
-        
-        
-        httpRequest.open('GET', 'https://rest.coinapi.io/v1/exchangerate/BTC', true);
-        httpRequest.setRequestHeader('X-CoinAPI-Key', '9E68560C-5F7A-402C-B131-3628228D254A');
-        httpRequest.send();
+
+        //Define que a função requestDone será chamdo quando a requisição for concluida
         httpRequest.onreadystatechange = requestDone
         
+        //Abre a conexão com a API usando o metodo GET
+        httpRequest.open('GET', 'https://rest.coinapi.io/v1/exchangerate/BTC', true);
+
+        //Configura um header de autorização, o primeiro parametro é o nome do parametro e o segundo a chave
+        //que foi fornecida para acessar a API
+        httpRequest.setRequestHeader('X-CoinAPI-Key', '9E68560C-5F7A-402C-B131-3628228D254A');
+
+        //Envia a requisição
+        httpRequest.send();
+        
+        //Será chamada assim que a requisição for concluida
         function requestDone(){
             const container = document.getElementById('container');
+
+            //Injeta na div container a resposta da requisição
             container.innerText = httpRequest.responseText;
         }
     }
